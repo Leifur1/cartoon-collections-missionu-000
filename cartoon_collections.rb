4e175386ba42308afb46a4c5aa57c6a8785d2283
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 23:39:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  return cheese_types if array.include?(cheese_types) 
+  return cheese_types if array.include?(cheese_types)
   end
 end
