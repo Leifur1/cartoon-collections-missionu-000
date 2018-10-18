7f6905a889c3b449724ecdcf7a375ce16b0ba63f
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 23:29:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |i|
+  planeteer_calls.collect do |calls|
     i.capitilize << "!"
   end
 end
