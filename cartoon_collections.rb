d92d9ad602e103f93d05ffda55b71bc5f0cebbc1
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-18 23:22:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,8 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |i|
     i.capitalize.join("! ")
+    i.split(" ")
+  end
 end
 
 def long_planeteer_calls# code an argument here
